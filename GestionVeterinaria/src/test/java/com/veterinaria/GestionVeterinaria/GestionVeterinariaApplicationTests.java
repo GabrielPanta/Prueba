528908feb13b298d1cb8b304bcda3d6a3402595b
gabrielpanta <gabrielpantajimenez@hotmail.com>
2025-04-26 01:18:24 -0500

    first commit

---
+++ GestionVeterinaria/src/test/java/com/veterinaria/GestionVeterinaria/GestionVeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinaria.GestionVeterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
